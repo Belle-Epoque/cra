fe8c39d397ca82282c9d073e0fd869766f6833d6
Clément Rousseau <clement@agence-belle-epoque.fr>
2020-02-06 16:30:19 +0100

    Add hooks API Call and Movie List

--- src/pages/Hooks/design.js
+++ src/pages/Hooks/design.js
@@ -1,9 +1,31 @@
-import React, { Fragment } from "react";
-
-const Hooks = ({ count, setCountOnClick }) => (
+import React, { Fragment } from 'react';
+const Hooks = ({
+  count,
+  setCountOnClick,
+  setQueryOnChange,
+  setSearchOnClick,
+  movieInfo,
+  isSearch
+}) => (
   <Fragment>
     <p>Vous avez cliqué {count} fois</p>
     <button onClick={setCountOnClick}>Cliquez ici</button>
+    <br />
+    <input type="text" onChange={setQueryOnChange} />
+    <button onClick={setSearchOnClick} disabled={isSearch}>
+      Rechercher
+    </button>
+    <br />
+    {/*     Add styled component ? */}
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {movieInfo &&
+        movieInfo.map((movie, index) => (
+          <div key={index} style={{ width: '33%' }}>
+            <h5>{movie.title}</h5>
+            <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} />
+          </div>
+        ))}
+    </div>
   </Fragment>
 );
 
